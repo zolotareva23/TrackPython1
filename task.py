92107507853c7f25dec353f66f934d889334a63c
zolotareva23 <zolotareva.aa@edu.spbstu.ru>
2025-12-10 21:13:14 +0300

    ЛР1

---
+++ task.py
@@ -0,0 +1,6 @@
+a = 123  # помещаем в переменную a объект со значением 123
+sq = 4  # присваиваем значение 4 в переменную sq
+
+# TODO Cоздать переменную some_variable и присвоить ей значение "Hello, World!"'
+some_variable = "Hello, World!"
+print(some_variable)
